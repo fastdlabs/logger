6d151d70d11bf5a03bedce21d5c9974096e5c9b3
JanHuang <bboyjanhuang@gmail.com>
2020-06-21 22:40:27 +0800

    init

---
+++ src/AccessHandler.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2016
+ *
+ * @see      https://www.github.com/janhuang
+ * @see      https://fastdlabs.com
+ */
+
+namespace FastD\Logger;
+
+/**
+ * Class AccessHandler.
+ */
+class AccessHandler extends HandlerAbstract
+{
+    /**
+     * @return array
+     */
+    protected function logContextFormat()
+    {
+        return [
+            'status' => response()->getStatusCode(),
+            'get' => request()->getQueryParams(),
+            'post' => request()->getParsedBody(),
+        ];
+    }
+}
